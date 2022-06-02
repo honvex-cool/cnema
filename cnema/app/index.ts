e06c01cc995a839d57a64d391a6e1466163b285c
Jakub Oskwarek <jakub@oskwarek.pl>
2022-06-03 01:08:55 +0200

    Configure TypeScript, express and dotenv

---
+++ cnema/app/index.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get(
+    '/',
+    (_: Request, res: Response) => {
+        res.send('Welcome to Cnema from TypeScript!');
+    }
+);
+
+app.listen(
+    port,
+    () => {
+        console.log(`[server] Server is running at port ${port}!`);
+    }
+)
