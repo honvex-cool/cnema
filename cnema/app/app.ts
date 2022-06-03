22001ec674f67648075f12aa4abe86b1b6dcbfcc
Jakub Oskwarek <jakub@oskwarek.pl>
2022-06-03 22:38:15 +0200

    Use node-postgres

--- cnema/app/app.ts
+++ cnema/app/app.ts
@@ -1,18 +1,23 @@
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
+import pg from 'pg'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+const db = new pg.Client('postgres://cnemaadmin:bazunia@localhost:5432/cnema');
+db.connect();
+
 app.set('view engine', 'ejs');
 
 app.get(
     '/',
-    (_req: Request, res: Response) => {
-        console.log('czemu to nie dziama')
-        return res.send('Dziama');
+    async (_request: Request, response: Response) => {
+        const data = await db.query('SELECT * FROM people');
+        const result = data.rows.map(row => row.first_name + ' ' + row.last_name).join('<br/>');
+        return response.send(`Found ${data.rowCount} entries in table people:<br/>${result}`);
     }
 );
 
