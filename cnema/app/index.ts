fde615e9e669496eebf77cf94c36e4104b26d4f8
Jakub Oskwarek <jakub@oskwarek.pl>
2022-06-03 13:28:38 +0200

    Add makeshift database connection and a simple query

--- cnema/app/index.ts
+++ cnema/app/index.ts
@@ -1,5 +1,10 @@
-import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
+
+import pgPromise from 'pg-promise';
+
+const pgp = pgPromise();
+const db = pgp('postgres://cnemaadmin:bazunia@localhost:5432/cnema');
 
 dotenv.config();
 
@@ -8,8 +13,11 @@ const port = process.env.PORT;
 
 app.get(
     '/',
-    (_: Request, res: Response) => {
-        res.send('Welcome to Cnema from TypeScript!');
+    async (_req: Request, res: Response) => {
+        let result = await db.manyOrNone('select * from people;').then(
+            data => data.map((row, index, _data) => String(index + 1) + ". " + row.first_name + ' ' + row.last_name).join('<br/>')
+        );
+        return res.send(`People present in database:<br/>${result}`);
     }
 );
 
