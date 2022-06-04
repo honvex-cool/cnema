76a7f5f551de7d4d22e68a2a4a268a9075719d8d
Jakub Oskwarek <jakub@oskwarek.pl>
2022-06-04 20:38:48 +0200

    Create basic GUI

--- cnema/app/app.ts
+++ cnema/app/app.ts
@@ -1,25 +1,31 @@
 import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
+import express from 'express';
 import pg from 'pg'
+import expressLayouts from 'express-ejs-layouts'
 
 dotenv.config();
 
-const app: Express = express();
+const app = express();
 const port = process.env.PORT;
 
 const db = new pg.Client('postgres://cnemaadmin:bazunia@localhost:5432/cnema');
 db.connect();
 
+app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
+app.set('layout', './layouts/user-layout');
+app.use(expressLayouts);
 
 app.get(
     '/',
-    async (_request: Request, response: Response) => {
-        const data = await db.query('SELECT * FROM people');
-        const result = data.rows.map(row => row.first_name + ' ' + row.last_name).join('<br/>');
-        return response.send(`Found ${data.rowCount} entries in table people:<br/>${result}`);
-    }
+    (_request, response) => response.redirect('/index')
 );
+
+app.get(
+    '/index',
+    (_request, response) => response.render('index')
+)
 
 app.listen(
     port,
