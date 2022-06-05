8acdc39cb16886d31d50273846222ad4f56daac5
Jakub Oskwarek <jakub@oskwarek.pl>
2022-06-05 11:47:15 +0200

    Replace the scary joins with functions that perform nested queries

--- cnema/db/movie_info.sql
+++ cnema/db/movie_info.sql
@@ -131,37 +131,126 @@ END;
 $$
 LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION format_movie_genres(movie integer)
+RETURNS text
+AS
+$$
+BEGIN
+    RETURN (
+        SELECT string_agg(genres.short_name, ', ')
+        FROM
+            movies_genres JOIN genres ON genres.genre_id = movies_genres.genre_id
+        WHERE movies_genres.movie_id = movie
+    );
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION format_movie_directors(movie integer)
+RETURNS text
+AS
+$$
+BEGIN
+    RETURN (
+        SELECT string_agg(format_person(directors), ', ')
+        FROM
+            movies_directors JOIN people directors ON directors.person_id = movies_directors.director_id
+        WHERE movies_directors.movie_id = movie
+    );
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION format_movie_screenwriters(movie integer)
+RETURNS text
+AS
+$$
+BEGIN
+    RETURN (
+        SELECT string_agg(format_person(screenwriters), ', ')
+        FROM
+            movies_screenwriters JOIN people screenwriters ON screenwriters.person_id = movies_screenwriters.screenwriter_id
+        WHERE movies_screenwriters.movie_id = movie
+    );
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION format_movie_actors(movie integer)
+RETURNS text
+AS
+$$
+BEGIN
+    RETURN (
+        SELECT string_agg(format_actor(actors, portraying), ', ')
+        FROM
+            movies_actors JOIN people actors ON actors.person_id = movies_actors.actor_id
+        WHERE movies_actors.movie_id = movie
+    );
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION format_movie_composers(movie integer)
+RETURNS text
+AS
+$$
+BEGIN
+    RETURN (
+        SELECT string_agg(format_person(composers), ', ')
+        FROM
+            movies_composers JOIN people composers ON composers.person_id = movies_composers.composer_id
+        WHERE movies_composers.movie_id = movie
+    );
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION format_movie_producers(movie integer)
+RETURNS text
+AS
+$$
+BEGIN
+    RETURN (
+        SELECT string_agg(company_name, ', ')
+        FROM
+            movies_producers JOIN producers ON producers.producer_id = movies_producers.producer_id
+        WHERE movies_producers.movie_id = movie
+    );
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION short_format_movie_reviews(movie integer)
+RETURNS text
+AS
+$$
+BEGIN
+    RETURN (
+        SELECT string_agg(short_format_review(reviews), '; ')
+        FROM
+            movies_reviews JOIN reviews ON reviews.review_id = movies_reviews.review_id
+        WHERE movies_reviews.movie_id = movie
+    );
+END;
+$$
+LANGUAGE plpgsql;
+
 CREATE OR REPLACE VIEW movie_info
 AS
 SELECT
-    movies.movie_id,
-    movies.title,
-    string_agg(DISTINCT genres.short_name, ', ')
+    movie_id,
+    title,
+    format_movie_genres(movie_id) AS genres,
     duration,
     age_rating,
     international_release AS release_date,
-    string_agg(DISTINCT language_name, ', ') AS original_language,
-    string_agg(DISTINCT format_person(directors), ', ') AS directed_by,
-    string_agg(DISTINCT format_person(screenwriters), ', ') AS screenplay_by,
-    string_agg(DISTINCT format_actor(actors, portraying), ', ') AS starring,
-    string_agg(DISTINCT format_person(composers), ', ') AS music_by,
-    string_agg(DISTINCT company_name, ', ') AS produced_by,
-    string_agg(DISTINCT short_format_review(reviews), '; ') AS reviewed_as
+    language_name AS original_language,
+    format_movie_directors(movie_id) AS directed_by,
+    format_movie_screenwriters(movie_id) AS screenplay_by,
+    format_movie_actors(movie_id) AS starring,
+    format_movie_composers(movie_id) AS music_by,
+    format_movie_producers(movie_id) AS produced_by,
+    short_format_movie_reviews(movie_id) AS reviewed_as
 FROM
-    movies
-        LEFT JOIN languages ON languages.language_id = movies.original_language
-        LEFT JOIN movies_genres ON movies_genres.movie_id = movies.movie_id
-        LEFT JOIN genres ON genres.genre_id = movies_genres.genre_id
-        LEFT JOIN movies_directors ON movies_directors.movie_id = movies.movie_id
-        LEFT JOIN people directors ON directors.person_id = movies_directors.director_id
-        LEFT JOIN movies_screenwriters ON movies_screenwriters.movie_id = movies.movie_id
-        LEFT JOIN people screenwriters ON screenwriters.person_id = movies_screenwriters.screenwriter_id
-        LEFT JOIN movies_actors ON movies_actors.movie_id = movies.movie_id
-        LEFT JOIN people actors ON actors.person_id = movies_actors.actor_id
-        LEFT JOIN movies_composers ON movies_composers.movie_id = movies.movie_id
-        LEFT JOIN people composers ON composers.person_id = movies_composers.composer_id
-        LEFT JOIN movies_producers ON movies_producers.movie_id = movies.movie_id
-        LEFT JOIN producers ON producers.producer_id = movies_producers.producer_id
-        LEFT JOIN movies_reviews ON movies_reviews.movie_id = movies.movie_id
-        LEFT JOIN reviews ON reviews.review_id = movies_reviews.review_id
-GROUP BY movies.movie_id;
+    movies LEFT JOIN languages ON languages.language_id = movies.original_language;
